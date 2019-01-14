fd1aa283789e0140915aebee27c2fb807b5e111f
Dennis Stolmeijer <dstollie@gmail.com>
2019-01-14 17:40:32 +0100

    Renamed basecamp.com to basecamp

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,7 +1,5 @@
-import ext from "./utils/ext";
-
 if(window.location.origin === 'https://3.basecamp.com') {
-  require('./sites/basecamp.com/contentscript');
+  require('./sites/basecamp/contentscript');
 }
 
 switch (window.location.origin) {
@@ -10,7 +8,7 @@ switch (window.location.origin) {
     break;
   case 'https://3.basecamp.com':
     document.body.className += ' site-basecamp';
-    require('./sites/basecamp.com/contentscript');
+    require('./sites/basecamp/contentscript');
     break;
   case 'https://favro.com':
       document.body.className += ' site-favro';
