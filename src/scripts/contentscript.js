99a01d2fc0ee07358cb8c561fecf4ffc98204c63
Rick Gout <rickgout@gmail.com>
2019-06-20 15:14:39 +0200

    Fixed class code

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,28 +1,28 @@
-if(window.location.origin === 'https://3.basecamp.com') {
-  require('./sites/basecamp/contentscript');
+if (window.location.origin === 'https://3.basecamp.com') {
+    require('./sites/basecamp/contentscript');
 }
 
 
 switch (window.location.origin) {
-  case 'https://tweakers.net':
-      document.body.className += ' site-tweakers';
-    break;
-  case 'https://3.basecamp.com':
-    document.body.className += ' site-basecamp';
-    require('./sites/basecamp/contentscript');
-    break;
-  case 'https://favro.com':
-      document.body.classList.add('site-favro');
-      break;
+    case 'https://tweakers.net':
+        document.body.classList.add('site-tweakers');
+        break;
+    case 'https://3.basecamp.com':
+        document.body.classList.add('site-basecamp');
+        require('./sites/basecamp/contentscript');
+        break;
+    case 'https://favro.com':
+        document.body.classList.add('site-favro');
+        break;
 }
 
 function updateFullScreenClass() {
-  if(!window.screenTop && !window.screenY && document.body.classList.contains('site-fullscreen') === false) {
-    document.body.classList.add('site-fullscreen');
-  }
+    if (!window.screenTop && !window.screenY && document.body.classList.contains('site-fullscreen') === false) {
+        document.body.classList.add('site-fullscreen');
+    }
 }
 
-window.addEventListener("resize", function(event) {
-  updateFullScreenClass();
+window.addEventListener("resize", function (event) {
+    updateFullScreenClass();
 });
 updateFullScreenClass();
